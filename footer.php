89d589b4841231851db09317ff692e5a6266fb01
Zeke Y <ezekiel@yarashus.com>
2014-12-02 20:24:56 -0500

    Updated the footer to use PHP magic and display a link if the user is logged in, and not if they aren't

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
         <footer>
             <div class="footer-nav">
 			    <a href="/about" class="footer-link" id="donate">About</a>
+                <?php
+                    if (is_user_logged_in()):
+                ?>
                 <a href="/wp-admin" class="footer-link" id="admin">Admin</a>
+                <?php
+                    else:
+                ?>
+                <a href="/wp-login.php?action=register" class="footer-link" id="admin">Register</a>
+                <?php endif; ?>
 			    <a href="/contact" class="footer-link" id="contact">Contact</a>
             </div>
 		
