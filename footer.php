22027c987cdb5c3ae960c086bde11347d6f6fe0a
Zeke Y <ezekiel@yarashus.com>
2014-12-03 09:39:29 -0500

    Updated the footer link admin text

--- footer.php
+++ footer.php
@@ -4,16 +4,16 @@
                 <?php
                     if (is_user_logged_in()):
                 ?>
-                <a href="/wp-admin" class="footer-link" id="admin">Admin</a>
+                <a href="/wp-admin" class="footer-link" id="admin">Write a review</a>
                 <?php
                     else:
                 ?>
-                <a href="/wp-login.php?action=register" class="footer-link" id="admin">Register</a>
+                <a href="/wp-login.php" class="footer-link" id="admin">Login</a>
                 <?php endif; ?>
 			    <a href="/contact" class="footer-link" id="contact">Contact</a>
             </div>
 		
-		    <p class="copyright-notice">&copy; Copyrighted and designed by Zeke Y 2014 | <a href="/wp-admin">Admin</a></p>
+		    <p class="copyright-notice">&copy; Copyrighted and designed by Zeke Y 2014</p>
         </footer>
     </body>
 </html>
