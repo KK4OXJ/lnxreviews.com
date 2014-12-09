93f3ca70d01487cbbcd1041ff51761830ffc69e3
Zeke Y <ezekiel@yarashus.com>
2014-12-09 11:10:58 -0500

    Working on the footer links

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
         <footer>
             <div class="footer-nav">
-			    <a href="/about" class="footer-link" id="about">About</a>
+			    <a href="/about" class="footer-link about">About</a>
                 <?php
                     if (is_user_logged_in()):
                 ?>
-                <a href="/wp-admin/edit.php" class="footer-link" class="admin">Write a review</a>
+                <a href="/wp-admin/edit.php" class="footer-link admin">Write a review</a>
                 <?php
                     else:
                 ?>
-                <a href="/wp-login.php" class="footer-link" class="admin">Login</a>
+                <a href="/wp-login.php" class="footer-link admin">Login</a>
                 <?php endif; ?>
-			    <a href="/contact" class="footer-link" class="contact">Contact</a>
+			    <a href="/contact" class="footer-link contact">Contact</a>
             </div>
 		
 		    <p class="copyright">&copy; Copyrighted and designed by Zeke Y 2014</p>
