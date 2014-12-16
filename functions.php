74f905d39c59aea7969fc6a79c3423f602b78531
Zeke Y <ezekiel@yarashus.com>
2014-12-16 16:23:38 -0500

    Adding the option to customize the background

--- functions.php
+++ functions.php
@@ -1,8 +1,13 @@
 <?php
-    $args = array(
+    $custom_header_defaults = array(
 	   'width' => 100,
 	   'default-image' => get_template_directory_uri() . '/tux.png',
 	   'uploads' => true,
     );
-    add_theme_support('custom-header', $args);
+    add_theme_support('custom-header', $custom_header_defaults);
+    
+    $custom_background_defaults = array(
+        'default-color' => '888888'
+    );
+    add_theme_support('custom-background', $custom_background_defaults);
 ?>
