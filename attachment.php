7663696fc258cf1b76c1702206c29387d3ddbefa
Zeke Y <ezekiel@yarashus.com>
2014-12-15 19:27:42 -0500

    Finally figured out how to make attachment.php display the attachment

--- attachment.php
+++ attachment.php
@@ -5,14 +5,14 @@
 <div class="post">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
  	<div class="universal-div">
- 		<?php the_content(); ?>
+ 		<?php the_attachment_link() ?>
  	</div>
 
  	</div>
 
     <?php endwhile; else: ?>
 
- 	<p><?php _e("Sorry, we couldn't find any attachments for you") ?></p>
+ 	<p><?php _e("Sorry, we couldn't find any attachmentss for you") ?></p>
 
     <?php endif; ?>
 </div>
