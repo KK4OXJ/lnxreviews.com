897e50b347a6bdaa75348a2d68deab30b387b8b2
Zeke Y <ezekiel@yarashus.com>
2014-12-18 22:00:55 -0500

    Fixed page-contact.php

--- page-contact.php
+++ page-contact.php
@@ -8,10 +8,7 @@ get_header() ?>
 
 <div class="post">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
- 	<div class="universal-div">
  		<?php the_content(); ?>
- 	</div>
-
     <?php endwhile; ?>
     <?php endif; ?>
 </div>
