4a94b12fab6e59384d891ff08a77de6ad870a95d
Zeke Y <ezekiel@yarashus.com>
2014-12-22 17:36:19 -0500

    Updated the viewport <meta> tag

--- header-home.php
+++ header-home.php
@@ -16,7 +16,7 @@
     <meta name="Keywords" content="linux operating systems, linux distro, linux distribution, linux reviews, linux, reviews">
     <meta name="author" content="Zeke Y">
     <meta http-equiv="cache-control" content="public" max-age="604800">
-    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" href="<?php echo esc_url(get_stylesheet_directory_uri()) ?>/crusoe-text.min.css">
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>">
