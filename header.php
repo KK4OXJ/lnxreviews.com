dcb1ff240cd981b4baa473d24b6792635947f65a
Zeke Y <ezekiel@yarashus.com>
2014-12-22 15:00:44 -0500

    Working on the caching

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
     <meta name="robots" content="index, follow, archive, cache, imageindex">
     <meta name="Keywords" content="linux operating systems linux distro distribution linux reviews">
     <meta name="author" content="Zeke Y">
-    <meta http-equiv="cache-control" content="public">
+    <meta http-equiv="cache-control" content="public" max-age="604800">
     <meta name=viewport content="width=device-width, initial-scale=1">
    
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>">
