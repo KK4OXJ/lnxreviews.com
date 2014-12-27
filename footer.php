c4974d8b07a979c4591d88fe53ed47fedbb2f8b4
Zeke Y <ezekiel@yarashus.com>
2014-12-27 17:09:02 -0500

    Tweaking the DNT stuff

--- footer.php
+++ footer.php
@@ -34,9 +34,9 @@
 
 <script>
   if (navigator.doNotTrack == 1) {
-    return;
+    console.log("We support Do Not Track (DNT)")
   }
-  else {
+  else if (navigator.doNotTrack == "unspecified") {
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
