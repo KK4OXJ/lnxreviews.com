358a36db855071d18b2ea1fa548bd19c13c77497
Zeke Y <ezekiel@yarashus.com>
2015-01-08 12:51:56 -0500

    Fixed a problem in 404.php

--- 404.php
+++ 404.php
@@ -3,12 +3,6 @@
 <h1 class="page-header">404</h1>
 <h2 class="small-header">Sorry, we couldn't find that page</h2>
 
-<?php
-  if (is_404()) {
-    _gaq.push(['_trackEvent', '404', document.location.pathname + document.location.search, document.referrer, 0, true]);
-  }
-?>
-
 <?php get_footer() ?>
 </body>
 </html>
