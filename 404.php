b96e42b883ad4d7acaf216b8c25e5d03b482bf56
Zeke Y <ezekiel@yarashus.com>
2015-01-08 19:10:26 -0500

    Added Google Analytics to the 404 page

--- 404.php
+++ 404.php
@@ -3,6 +3,12 @@
 <h1 class="page-header">404</h1>
 <h2 class="small-header">Sorry, we couldn't find that page</h2>
 
+<?php if (is_404()): ?>
+  <script>
+    _gaq.push(['_trackEvent', '404', document.location.pathname + document.location.search, document.referrer, 0, true]);
+  </script>
+<?php endif; ?>
+
 <?php get_footer() ?>
 </body>
 </html>
